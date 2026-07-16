4f87e140ae04a5bbb0a5fe7147c2710d2f73df16
Chris Lo <chris.aa.lo@gmail.com>
2026-07-16 10:13:10 -0700

    plot

--- TCGA_analysis Updated June2026.R
+++ TCGA_analysis Updated June2026.R
@@ -40,7 +40,9 @@ normalized_counts <- counts(dds, normalized = TRUE)
 source("TCGA_analysis Function 2026.R")
 GeneNames = c("ADAMTS2", "ADAMTSL4", "ANGPTL4", "ASPN", "BMP1", "CD109", "COL12A1", "COL14A1", "COL16A1", "COL1A1", "COL1A2", "COL5A2", "COL6A1", "COL6A2", "COL8A1", "CRLF1", "DPT", "EMILIN1", "FBN1", "FN1", "LOX", "LOXL2", "LTBP1", "MFAP2", "MFAP5", "MMP19", "POSTN", "SERPINE1", "TGFBI", "THBS1", "THBS2", "THSD4", "TIMP1", "TNC","TSKU","VWA1")
 results <- map(GeneNames, SingleGeneSurvivalAnalysis, data_se, normalized_counts)
-#save plots and tables code missing?...
+pdf(file = "SingleGeneSurvivalAnalysis.pdf")
+map(results, 1)
+dev.off()
 
 #Analysis 2: CD8 boxplots
 source("CD8_TCGA Function.R")
@@ -60,7 +62,7 @@ dev.off()
 
 #Analysis 4: CD68 boxplots
 source("CD68_TCGA Function.R")
-CD68combined_boxplots <- map(GeneNames, MacroExpr_Comparison)
+CD68combined_boxplots <- map(GeneNames, MacroExpr_Comparison, data_se, normalized_counts)
 pdf(file = "CD68combined_boxplots.pdf")
 par(mfrow = c(2,2))
 CD68combined_boxplots
@@ -70,5 +72,6 @@ dev.off()
 #Analysis 5: Composite Gene Score Survival Analysis 
 source("Updated Composite Score and OS.R")
 composite_analysis <- compositeSurvivalAnalysis(data_se, normalized_counts)
-#save plots and tables code missing?...
-
+pdf(file = "CompositeSurvivalAnalysis.pdf")
+composite_analysis$plot
+dev.off()
